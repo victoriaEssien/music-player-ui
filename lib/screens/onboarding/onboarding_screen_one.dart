f11172b82d28c1c594f72ff44c0d59543e91f804
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2022-12-29 23:33:29 +0100

    initial commit

---
+++ lib/screens/onboarding/onboarding_screen_one.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
